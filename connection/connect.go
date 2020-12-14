e6e86b2139ede2002d28e1e2dd632242c7ff1b01
Haydn Appleby <haydnba@pm.me>
2020-12-14 00:23:08 +0000

    connection handler now integrated with aws ddb client

--- connection/connect.go
+++ connection/connect.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
+// Item - DDB item type
+type Item struct {
+	ConnectionID string
+}
 
 // Response - will export to lib soon
 type Response = events.APIGatewayProxyResponse
@@ -15,15 +26,41 @@ func main() {
 	lambda.Start(handler)
 }
 
-func handler(req *events.APIGatewayWebsocketProxyRequest) (Response, error) {
-	// Just do this for now...
-	if false {
-		return Response{StatusCode: http.StatusInternalServerError}, nil
-	}
-
-	// Log some stuff
+func handler(_ context.Context, req *events.APIGatewayWebsocketProxyRequest) (Response, error) {
+	// Log execution
 	log.Printf("Processing Lambda request %s\n", req.RequestContext.RequestID)
 	log.Printf("Processing Lambda request %s\n", req.RequestContext.ConnectionID)
+
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	item := Item{
+		ConnectionID: req.RequestContext.ConnectionID,
+	}
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		log.Fatalf("Error processing connection id: %v", err)
+		return Response{StatusCode: http.StatusInternalServerError}, err
+	}
+
+	table := os.Getenv("TABLE_NAME")
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(table),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Fatalf("Error storing connection id: %v", err)
+		return Response{StatusCode: http.StatusInternalServerError}, err
+	}
 
 	// Return OK
 	return Response{StatusCode: http.StatusOK}, nil
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/haydnba/golong
 
 go 1.13
 
-require github.com/aws/aws-lambda-go v1.20.0
+require (
+	github.com/aws/aws-lambda-go v1.20.0
+	github.com/aws/aws-sdk-go v1.36.7
+	github.com/aws/aws-sdk-go-v2 v0.30.0
+	github.com/aws/aws-sdk-go-v2/config v0.3.0
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v0.30.0
+)
